aaf6c9622d45e27d8a69bdb615ac2132f51bbd63
Michael Bardou <michael.bardou@icloud.com>
2022-02-10 17:26:52 +0100

    create and prepare Wizard class

--- src/main/java/labyGame/Wizard.java
+++ src/main/java/labyGame/Wizard.java
@@ -1,2 +1,36 @@
-package labyGame;public class Wizard {
+package labyGame;
+
+public class Wizard extends Character {
+
+    //All args constructor
+    public Wizard(int Hp, characterState status,String Name,int Power){
+        super(Hp,Name, Power, status);
+    }
+
+    //Basic status
+    public Wizard(int Hp, String name, int power){
+        super(Hp,name,power);
+    }
+
+    @Override
+    public void basicAttack(Character target) {
+        //A remplir
+    }
+
+    @Override
+    public String toString() {
+        //A remplir
+        return null;
+    }
+
+    @Override
+    public String basicTalk(String sentence) {
+        //a remplir
+        return null;
+    }
+
+    @Override
+    public void secretAttack(Character target) {
+        //a remplir
+    }
 }
