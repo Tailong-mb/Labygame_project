acf5b1228283d103fd282b1de94868383a2cc09c
Michael Bardou <michael.bardou@icloud.com>
2022-02-10 17:22:37 +0100

    Prepare monster class

--- src/main/java/labyGame/Monster.java
+++ src/main/java/labyGame/Monster.java
@@ -1,2 +1,31 @@
-package labyGame;public class Monster {
+package labyGame;
+
+public class Monster extends Character{
+
+    //All args constructor
+    public Monster(int Hp, characterState status,String Name,int Power){
+        super(Hp,Name, Power, status);
+    }
+
+    @Override
+    public void basicAttack(Character target) {
+        //A remplir
+    }
+
+    @Override
+    public String toString() {
+        //A remplir
+        return null;
+    }
+
+    @Override
+    public String basicTalk(String sentence) {
+        //a remplir
+        return null;
+    }
+
+    @Override
+    public void secretAttack(Character target) {
+        //a remplir
+    }
 }
