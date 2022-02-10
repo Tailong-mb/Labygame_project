aeb0b7096d07bc95bbd8a064a17e2669f27a7f65
Michael Bardou <michael.bardou@icloud.com>
2022-02-10 17:22:20 +0100

    add get and set method

--- src/main/java/labyGame/Character.java
+++ src/main/java/labyGame/Character.java
@@ -1,9 +1,9 @@
 package labyGame;
 
-import java.util.Locale;
+import java.util.Objects;
 
 public abstract class Character {
-    protected int Hp;
+    protected int hp;
     protected characterState currentStatus;
     protected String name;
     protected int power;
@@ -11,7 +11,7 @@ public abstract class Character {
 
     //All args constructor
     public Character(int Hp, String name, int power, characterState currentStatus){
-        this.Hp = Hp;
+        this.hp = Hp;
         this.currentStatus = currentStatus;
         this.name = name;
         this.power = power;
@@ -19,7 +19,7 @@ public abstract class Character {
 
     //Basic constructor
     public Character(int Hp, String name, int power){
-        this.Hp = Hp;
+        this.hp = Hp;
         this.currentStatus = characterState.NORMAL;
         this.name = name;
         this.power = power;
@@ -35,17 +35,78 @@ public abstract class Character {
     }
 
     /**
+     * Check is the character is dead or not.
+     * @return yes if Hp are under or equal to 0 otherwise false;
+     */
+    public boolean isDead(){
+        return this.hp <= 0;
+    }
+
+    /**
      * Principal method for a character to attack
      * @param target of the attack
      */
     public abstract void basicAttack(Character target);
 
     /**
-     * Secret attack method (should have a cooldown)
+     * Secret attack method (should have a cool down)
      * @param target of the secret attack
      */
     public abstract void secretAttack(Character target);
 
+    /**
+     * Charter will talk with some specificities
+     * @param sentence that will be changed
+     * @return the same sentence after change.
+     */
+    public abstract String basicTalk(String sentence);
+
     //Method toString, the character will present himself.
+    @Override
     public abstract String toString();
+
+    //Getter and Setter method
+    public int getHp() {
+        return hp;
+    }
+
+    public characterState getCurrentStatus() {
+        return currentStatus;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCurrentStatus(characterState currentStatus) {
+        this.currentStatus = currentStatus;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Character character)) return false;
+        return getPower() == character.getPower() && getName().equals(character.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getPower());
+    }
 }
---
+++ src/main/java/labyGame/Monster.java
@@ -0,0 +1,2 @@
+package labyGame;public class Monster {
+}
