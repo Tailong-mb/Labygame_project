3dac77ca93df5e4ed37bd07bf3bfc6eea546fe7f
Michael Bardou <michael.bardou@icloud.com>
2022-02-11 08:30:20 +0100

    add hero class

---
+++ src/main/java/labyGame/Hero.java
@@ -0,0 +1,67 @@
+package labyGame;
+
+import java.util.HashMap;
+
+public class Hero extends Role {
+    private HashMap<String,Integer> myItem;
+    private int positionX;
+    private int positionY;
+
+    //All args constructor
+    public Hero(int hp, CharacterState status, String name, int power, int positionX, int positionY, HashMap<String, Integer> myItem) {
+        super(hp,name, power, status);
+        this.myItem = myItem;
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    @Override
+    public void basicAttack(Role target) {
+        //A remplir
+    }
+
+    @Override
+    public String toString() {
+        //A remplir
+        return null;
+    }
+
+    @Override
+    public String basicTalk(String sentence) {
+        //a remplir
+        return null;
+    }
+
+    @Override
+    public void secretAttack(Role target) {
+        //a remplir
+    }
+
+    //get and set method
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public HashMap<String, Integer> getMyItem() {
+        return myItem;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public void setMyItem(HashMap<String, Integer> myItem) {
+        this.myItem = myItem;
+    }
+
+}
+
