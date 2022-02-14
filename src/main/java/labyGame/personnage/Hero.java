31545d88c37b54f2860c083d7d1088274190d55f
Michael Bardou <michael.bardou@icloud.com>
2022-02-14 22:16:29 +0100

    implementation of hero method

--- src/main/java/labyGame/personnage/Hero.java
+++ src/main/java/labyGame/personnage/Hero.java
@@ -1,9 +1,11 @@
 package labyGame.personnage;
 
 import labyGame.items.Item;
+import labyGame.items.ItemName;
 
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Hero extends Role implements Serializable {
     private HashMap<Item,Integer> myItem;
@@ -18,24 +20,48 @@ public class Hero extends Role implements Serializable {
 
     @Override
     public void basicAttack(Role target) {
-        //A remplir
+        Random rand = new Random();
+        target.setHp(-rand.nextInt(power - power/2+2) + power/2+2);
     }
 
     @Override
     public String toString() {
-        //A remplir
-        return null;
+        return String.format("My name is %s, I'm the hero who will defeat this labyrinth !", name);
     }
 
     @Override
     public String basicTalk(String sentence) {
-        //a remplir
-        return null;
+        return sentence.substring(0,1).toUpperCase() + toString().substring(1);
     }
 
     @Override
     public void secretAttack(Role target) {
-        //a remplir
+        Random rand = new Random();
+        if (CharacterState.NORMAL == currentStatus)
+            target.setHp(-rand.nextInt(power*power - power*10) + power *10);
+        else
+            hp -= power; //He hurts himself because he can't use his secret attack (not the good statement).
+    }
+
+    /**
+     * Check if the item can be used.
+     * @param itemTargeted the item who will be used if it's possible.
+     * @return true if it's possible else false.
+     */
+    public boolean canUseItem(Item itemTargeted){
+        return myItem.get(itemTargeted) > 0;
+    }
+
+    /**
+     * When canUseItem we can use this method to use this same item.
+     * @param itemTargeted the item the hero wants to use.
+     */
+    public void useItem(Item itemTargeted){
+        if (itemTargeted.name() == ItemName.ANTIDOTE)
+            currentStatus = CharacterState.NORMAL;
+        else
+            hp += itemTargeted.bonus();
+        myItem.put(itemTargeted, myItem.get(itemTargeted) - 1);
     }
 
     //get and set method
