5ab5e53ee2688db8bbf7bf590d8649a60339f4fd
Michael Bardou <95377183+Tailong-mb@users.noreply.github.com>
2022-02-15 16:26:17 +0100

    Update Riddles.java
    Try to respect camelCase. I changed one method ( the one one check if the answer is right or not) check it and commit again.

--- src/main/java/labyGame/riddles/Riddles.java
+++ src/main/java/labyGame/riddles/Riddles.java
@@ -2,44 +2,34 @@ package labyGame.riddles;
 
 public enum Riddles {
 
-    Riddle_One("What has to be broken before you can use it?", "an egg"),
-    Riddle_Two("I’m tall when I’m young, and I’m short when I’m old. What am I?", "a candle"),
-    Riddle_Three("What is full of holes but still holds water?", "a sponge"),
-    Riddle_Four("What is always in front of you but can’t be seen?", "the Future"),
-    Riddle_Five("What can you break, even if you never pick it up or touch it?", "a promise"),
-    Riddle_Six("You walk into a room that contains a match, a kerosene lamp, a candle and a fireplace. What would you light first?", "the match"),
-    Riddle_Seven("What can’t talk but will reply when spoken to?", "an echo"),
-    Riddle_Eight("The more of this there is, the less you see. What is it?", "Darkness"),
-    Riddle_Nine("I follow you all the time and copy your every move, but you can’t touch me or catch me. What am I?", "your shadow"),
-    Riddle_Ten("What gets bigger when more is taken away?", "a hole"),
-    Riddle_Eleven("Where does today come before yesterday?", "the dictionary"),
-    Riddle_Twelve("If you’ve got me, you want to share me; if you share me, you haven’t kept me. What am I?", "a secret");
+    riddleOne("What has to be broken before you can use it?", "an egg"),
+    riddleTwo("I’m tall when I’m young, and I’m short when I’m old. What am I?", "a candle"),
+    riddleThree("What is full of holes but still holds water?", "a sponge"),
+    riddleFour("What is always in front of you but can’t be seen?", "the Future"),
+    riddleFive("What can you break, even if you never pick it up or touch it?", "a promise"),
+    riddleSix("You walk into a room that contains a match, a kerosene lamp, a candle and a fireplace. What would you light first?", "the match"),
+    riddleSeven("What can’t talk but will reply when spoken to?", "an echo"),
+    riddleEight("The more of this there is, the less you see. What is it?", "Darkness"),
+    riddleNine("I follow you all the time and copy your every move, but you can’t touch me or catch me. What am I?", "your shadow"),
+    riddleTen("What gets bigger when more is taken away?", "a hole"),
+    riddleEleven("Where does today come before yesterday?", "the dictionary"),
+    riddleTwelve("If you’ve got me, you want to share me; if you share me, you haven’t kept me. What am I?", "a secret");
 
-    final String Rid;
-    final String Ans;
+    final String aid;
+    final String ans;
     String answer;
 
-    Riddles(String Rid, String Ans) {
-        this.Rid = Rid;
-        this.Ans = Ans;
+    Riddles(String rid, String ans) {
+        this.rid = rid;
+        this.ans = ans;
     }
 
+    public boolean ansVerification(String answer, Riddles question) {
+        return question.getAns().equals(answer);
+    }
+    
     public String getRid() {
         return Rid;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public boolean AnsRid() {
-        for (Riddles Ans : Riddles.values()) {
-            if (Ans.Rid.equalsIgnoreCase(answer))
-                return true;
-            else
-                return false;
-        }
-        return false;
     }
 
     public String getAns(){
