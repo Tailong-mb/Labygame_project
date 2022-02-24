fafb2de7b42c6b9a0e042be89968bcfd84a18037
TangoAlphaN <parissenathalie@orange.fr>
2022-02-24 17:27:55 +0100

    update @override
    I was thinking I put them accidently

--- src/main/java/labyGame/trayEnvironnement/Decor/Tree.java
+++ src/main/java/labyGame/trayEnvironnement/Decor/Tree.java
@@ -4,14 +4,17 @@ import labyGame.personnage.Hero;
 
 public class Tree implements DecorInterface {
 
+    @Override
     public boolean isDestroyed(){
         return false;
     }
 
+    @Override
     public boolean canMove() {
         return false;
     }
 
+    @Override
     public void hurtHero(Hero hero) {
         hero.setHp(0);
     }
