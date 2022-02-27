75a33912f0fc38aec07a03a692dba9963af3fb8e
Michael Bardou <michael.bardou@icloud.com>
2022-02-27 18:25:55 +0100

    correct ansVerification

--- src/main/java/labyGame/personnage/Wizard.java
+++ src/main/java/labyGame/personnage/Wizard.java
@@ -62,6 +62,6 @@ public class Wizard extends Role {
      * @return true if it's right else false.
      */
     public boolean ansVerification(String answer) {
-        return answer.matches(String.format(("(.*)%s(.*)"),currentRiddle.getAns()));
+        return answer.toLowerCase().matches(String.format(("(.*)%s(.*)"),currentRiddle.getAns().toLowerCase()));
     }
 }
