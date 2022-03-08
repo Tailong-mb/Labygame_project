b407700f2eb682e938bcdd22f1407fe2aea1a239
TangoAlphaN <parissenathalie@orange.fr>
2022-03-08 23:28:13 +0100

    Create musicPlayer.java

---
+++ src/main/java/labyGame/sound/musicPlayer.java
@@ -0,0 +1,49 @@
+package labyGame.sound;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class musicPlayer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Pane root = new Pane();
+        root.setPrefSize(1332, 850);
+
+        InputStream is = Files.newInputStream(Paths.get("doc/music/sound/cover.png"));
+        Image img = new Image(is);
+        is.close();
+        ImageView imgView = new ImageView(img);
+        imgView.setFitWidth(1332);
+        imgView.setFitHeight(850);
+
+        Media media = new Media(new File("doc/music/sound/GamerInstincts.wav").toURI().toString()));
+
+        MediaPlayer mp = new MediaPlayer(media);
+
+        mp.setAutoPlay(true);
+
+        root.getChildren().addAll(imgView, mp);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
