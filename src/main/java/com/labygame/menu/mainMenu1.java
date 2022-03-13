ae40c7bb557a715185359d10d618e7f71083a4ef
TangoAlphaN <parissenathalie@orange.fr>
2022-03-14 00:42:42 +0100

    Create mainMenu1.java

---
+++ src/main/java/com/labygame/menu/mainMenu1.java
@@ -0,0 +1,47 @@
+package com.labygame.menu;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class mainMenu1 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    GameMenu gameMenu;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Pane root = new Pane();
+        root.setPrefSize(1200, 850);
+
+        InputStream is = Files.newInputStream(Paths.get("doc/images/wallpaper/mainMenu1.jpg"));
+        Image img = new Image(is);
+        is.close();
+
+        ImageView imgView = new ImageView(img);
+        imgView.setFitWidth(1200);
+        imgView.setFitHeight(850);
+
+        gameMenu = new GameMenu();
+
+        root.getChildren().addAll(imgView, gameMenu);
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
