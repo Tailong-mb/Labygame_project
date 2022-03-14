745d5b215e7e2b6cdebaeed55bf1840a9a897e1c
Michael Bardou <michael.bardou@icloud.com>
2022-03-14 20:53:56 +0100

    add character image (draw)

--- src/main/java/com/labygame/demo/scenes/FightScene.java
+++ src/main/java/com/labygame/demo/scenes/FightScene.java
@@ -1,6 +1,7 @@
 package com.labygame.demo.scenes;
 
 import com.labygame.personnage.Hero;
+import com.labygame.personnage.Monster;
 import com.labygame.personnage.Role;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
@@ -8,6 +9,11 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 @Setter
 @Getter
@@ -38,6 +44,7 @@ public class FightScene extends GeneralScene{
 
         //Init Font
         Font myFontStats = Font.font("Arial", FontWeight.BOLD, 24);
+        gc.setFont(myFontStats);
 
         //Draw BackGround
         gc.drawImage(backgroundImage,0,0);
@@ -45,13 +52,47 @@ public class FightScene extends GeneralScene{
         //Draw Hero Stats
         Image hpImage = new Image("file:doc/images/gfx/gfx/fightScene/hpImage.png");
         Image swordImage = new Image("file:doc/images/gfx/gfx/fightScene/swordnormal.png");
+        Image statusImage = new Image("file:doc/images/gfx/gfx/fightScene/strenght.png");
         gc.drawImage(hpImage,20,50);
         gc.drawImage(swordImage,20,100);
+        gc.drawImage(statusImage,20,150);
 
-        gc.setFont(myFontStats);
         gc.setFill(Color.WHITE);
         gc.fillText(String.format("%2d",hero.getHp()),65,75);
         gc.fillText(String.format("%d",hero.getPower()),65,122);
+        gc.fillText(hero.getCurrentStatus().getNameState(),65,175);
 
+        //Draw opponent Stats
+        gc.drawImage(hpImage,1135,50);
+        gc.drawImage(swordImage,1135,100);
+        gc.drawImage(statusImage,1135,150);
+
+        gc.fillText(String.format("%2d",opponent.getHp()),1095,75);
+        gc.fillText(String.format("%d",opponent.getPower()),1095,122);
+        gc.fillText(opponent.getCurrentStatus().getNameState(),1020,175);
+
+        //Draw Monster
+        if(opponent instanceof Monster){
+            ArrayList<String> possibleMonsterList = new ArrayList<>(List.of(
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/chainBeast.png",
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/dracomachina.png",
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/eelWielder.png",
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/fishbone.png",
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/shredSquid.png",
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/strangeGhost.png"));
+
+            Random myRandom = new Random();
+            int randomPath = myRandom.nextInt(6);
+            Image monsterImage = new Image(possibleMonsterList.get(randomPath));
+            gc.drawImage(monsterImage,950,450);
+        }else{
+            //draw wizard
+            Image wizardImage = new Image("file:doc/images/gfx/gfx/fightScene/wizardRdyToFight.png");
+            gc.drawImage(wizardImage,900,400);
+        }
+
+        //Draw Hero
+        Image myHero = new Image("file:doc/images/gfx/gfx/fightScene/heroRdyToFight.png");
+        gc.drawImage(myHero,-100,275);
     }
 }
