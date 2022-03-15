bbc92b4960a5bab5df92a20bc9aaa329ce0955b8
Michael Bardou <michael.bardou@icloud.com>
2022-03-15 15:49:03 +0100

    update Itemname

--- src/main/java/com/labygame/items/ItemName.java
+++ src/main/java/com/labygame/items/ItemName.java
@@ -1,9 +1,9 @@
 package com.labygame.items;
 
-public enum ItemName {
+public enum ItemName{
     ANTIDOTE, // to be use when the character is poison
     HEALPOTION, // to be use when the character is sick
     ENERGYDRINK, // to be use when the character is weary
     KEY,
-    MAGICKEY;
+    MAGICKEY
 }
