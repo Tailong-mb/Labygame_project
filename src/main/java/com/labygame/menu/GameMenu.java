edd04cd54debd34d003fc24c0825be7c6b2aae9e
Nathalie PARISSE <parissenathalie@orange.fr>
2022-03-16 11:21:06 +0100

    update

--- src/main/java/com/labygame/menu/GameMenu.java
+++ src/main/java/com/labygame/menu/GameMenu.java
@@ -1,17 +1,27 @@
 package com.labygame.menu;
 
+import com.labygame.personnage.CharacterState;
+import com.labygame.personnage.Hero;
 import com.labygame.sound.Music;
 import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class GameMenu extends Parent {
 
     Music msc = new Music();
+    Hero newHero;
+    Hero oldHero;
 
     public GameMenu() {
 
@@ -47,6 +57,40 @@ public class GameMenu extends Parent {
             ft.setToValue(0);
             ft.setOnFinished(evt -> this.setVisible(false));
             ft.play();
+
+            StackPane secondaryLayout = new StackPane();
+
+            Scene secondScene = new Scene(secondaryLayout, 230, 100);
+
+            // New window (Stage)
+            Stage newWindow = new Stage();
+            newWindow.setTitle("Second Stage");
+            newWindow.setScene(secondScene);
+
+            newWindow.initModality(Modality.WINDOW_MODAL);
+
+            newWindow.setAlwaysOnTop(true);
+
+            Label label = new Label("Name your hero ");
+
+            TextField txtField = new TextField();
+            label.setTranslateY(-25);
+            final String[] nameHero = new String[1];
+
+            //Enter button creation
+            button btnEnter = new button("Enter");
+            btnEnter.setTranslateY(30);
+            btnEnter.setOnMouseClicked(evt -> {
+                nameHero[0] = txtField.getText();
+                newWindow.close();
+            });
+
+            newHero = new Hero(100, CharacterState.NORMAL, nameHero[0], 20, 50, 50, null);
+
+            secondaryLayout.getChildren().addAll(label, txtField, btnEnter);
+
+            newWindow.setTitle("New Hero");
+            newWindow.show();
 
         });
 
