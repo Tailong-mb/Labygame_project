c2e56623446130ae751635fc79f91d2f24153a6b
Michael Bardou <michael.bardou@icloud.com>
2022-03-17 21:10:46 +0100

    clean module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,14 @@
 module com.labygame{
+    requires static lombok;
     requires javafx.controls;
     requires javafx.fxml;
-    requires static lombok;
     requires org.jetbrains.annotations;
     requires com.google.gson;
 
     opens com.labygame.demo to javafx.fxml;
+    opens com.labygame.demo.scenes to javafx.fxml;
     exports com.labygame.demo;
     exports com.labygame.demo.scenes;
-    opens com.labygame.demo.scenes to javafx.fxml;
     exports com.labygame.personnage;
     exports com.labygame.items;
 
