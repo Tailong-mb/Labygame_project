418f3c2291edc3248ca8d340de11abd4e3797565
TangoAlphaN <parissenathalie@orange.fr>
2022-03-17 23:20:50 +0100

    Update GMenu.java

--- src/main/java/com/labygame/menu/GMenu.java
+++ src/main/java/com/labygame/menu/GMenu.java
@@ -1,5 +1,6 @@
 package com.labygame.menu;
 
+import com.labygame.mainLabyGame;
 import com.labygame.sound.Music;
 import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
@@ -11,7 +12,7 @@ import javafx.util.Duration;
 
 public class GMenu extends Parent {
 
-    Music msc = new Music();
+    Music msc = new Music(); //msc."music of the game scene".getMusic();
 
     public GMenu() {
 
@@ -41,11 +42,25 @@ public class GMenu extends Parent {
             ft.play();
         });
 
-        button btnR = new button("Return Main Menu");
-        //btnR.setOnMouseClicked(event -> {
-        //TODO
-        //});
+        //"Save" button
+        button btnSave = new button("Save");
+        btnSave.setOnMouseClicked(event -> {
+            //TODO
+        });
 
+        //"Glossary" button
+        button btnGlossary = new button("Glossary");
+        btnGlossary.setOnMouseClicked(event ->{
+            mainLabyGame.setScene(5);
+        });
+
+        //"Return Main menu" button
+        button btnR = new button("Return Main Menu");
+        btnR.setOnMouseClicked(event -> {
+            mainLabyGame.setScene(0);
+        });
+
+        //"Options" button
         button btnOpt = new button("Options");
         btnOpt.setOnMouseClicked(evnt -> {
             getChildren().add(menuOption);
@@ -62,9 +77,11 @@ public class GMenu extends Parent {
             tt.setOnFinished(evt -> getChildren().remove(menuMain));
         });
 
+        //"Exit" button to exit the game
         button btnE = new button("Exit");
         btnE.setOnMouseClicked(event -> System.exit(0));
 
+        //"Back" button
         button btnBk = new button("Back");
         btnBk.setOnMouseClicked(event -> {
             getChildren().add(menuMain);
@@ -81,6 +98,7 @@ public class GMenu extends Parent {
             tt.setOnFinished(evt -> getChildren().remove(menuSoundOption));
         });
 
+        //"Sound" button
         button btnS = new button("Sound");
         btnS.setOnMouseClicked(event -> {
             getChildren().add(menuSoundOption);
@@ -98,22 +116,19 @@ public class GMenu extends Parent {
 
         });
 
+        //"Mute" button to stop the music
         button btnM = new button("Mute");
-        //btnM.setOnMouseClicked(event ->
-        /**
-         * music."music of the game scene".getMusic();
-         * music.stopMusic();
-         */
-        //);
+        btnM.setOnMouseClicked(event ->msc.stopMusic());
 
+        //"Unmute" button to play the music if it was muted
         button btnU = new button("Unmute");
         btnU.setOnMouseClicked(event -> msc.playMusic());
 
-        menuMain.getChildren().addAll(btnRes, btnOpt, btnR, btnE);
+        menuMain.getChildren().addAll(btnRes, btnSave, btnGlossary, btnOpt, btnR, btnE);
         menuOption.getChildren().addAll(btnS);
         menuSoundOption.getChildren().addAll(btnM, btnU, btnBk);
 
-        Rectangle r = new Rectangle(1200, 850);
+        Rectangle r = new Rectangle(1200, 600);
         r.setFill(Color.GREY);
         r.setOpacity(0.4);
 
