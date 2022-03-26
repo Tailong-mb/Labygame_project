c88c3a8f671fa3e20f1019b292f3afe41d4daba3
Michael Bardou <michael.bardou@icloud.com>
2022-03-26 08:44:45 +0100

    set hero name (new game) finished

--- src/main/java/com/labygame/front/scenes/GameScene.java
+++ src/main/java/com/labygame/front/scenes/GameScene.java
@@ -1,17 +1,10 @@
 package com.labygame.front.scenes;
 
-import com.labygame.front.Labygame;
-import javafx.scene.input.KeyCode;
-
 public class GameScene extends GeneralScene{
 
     @Override
     public void draw() {
         activeKeys.clear();
 
-        if(activeKeys.contains(KeyCode.ESCAPE)){
-            Labygame.exit();
-            //Labygame.setScene(Labygame.MENU_SCENE);
-        }
     }
 }
