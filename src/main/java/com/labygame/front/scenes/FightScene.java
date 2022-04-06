88a05b15a5c997f5555562388df929b8ee77cb7f
Michael Bardou <michael.bardou@icloud.com>
2022-04-06 21:38:47 +0200

    divides the main function

--- src/main/java/com/labygame/front/scenes/FightScene.java
+++ src/main/java/com/labygame/front/scenes/FightScene.java
@@ -55,6 +55,9 @@ public class FightScene extends GeneralScene{
         this.lastHpOpponent = opponent.getHp();
     }
 
+    /**
+     * Draw the fight scene.
+     */
     @Override
     public void draw() {
         //Reset Key
@@ -96,7 +99,7 @@ public class FightScene extends GeneralScene{
      * @param opponent a wizard.
      */
     public void gameChoiceWizard(Wizard opponent){
-        //Textquesiton
+        //TextRiddle
         Text myText  = new Text(opponent.askQuestion());
         myText.setFont(Font.font(20));
         myText.setFill(Color.BLACK);
@@ -170,93 +173,15 @@ public class FightScene extends GeneralScene{
         //Draw BackGround
         gc.drawImage(backgroundImage,0,0);
 
-        //Draw Hero Stats
-        Image hpImage = new Image("file:doc/images/gfx/gfx/fightScene/hpImage.png");
-        Image swordImage = new Image("file:doc/images/gfx/gfx/fightScene/swordnormal.png");
-        Image statusImage = new Image("file:doc/images/gfx/gfx/fightScene/strenght.png");
-        gc.drawImage(hpImage,20,50);
-        gc.drawImage(swordImage,20,100);
-        gc.drawImage(statusImage,20,150);
+        drawHeroStats();
 
-        gc.setFill(Color.WHITE);
-        gc.fillText(String.format("%2d",hero.getHp()),65,75);
-        gc.fillText(String.format("%d",hero.getPower()),65,122);
-        gc.fillText(hero.getCurrentStatus().getNameState(),65,175);
-        gc.fillText(hero.getName(), 170,350);
-
-        //Draw opponent Stats
-        gc.drawImage(hpImage,1135,50);
-        gc.drawImage(swordImage,1135,100);
-        gc.drawImage(statusImage,1135,150);
-
-        gc.fillText(String.format("%2d",opponent.getHp()),1095,75);
-        gc.fillText(String.format("%d",opponent.getPower()),1095,122);
-        gc.fillText(opponent.getCurrentStatus().getNameState(),1020,175);
-
-        //Draw Monster
-        if(opponent instanceof Monster){
-            ArrayList<String> possibleMonsterList = new ArrayList<>(List.of(
-                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/chainBeast.png",
-                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/dracomachina.png",
-                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/eelWielder.png",
-                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/fishbone.png",
-                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/shredSquid.png",
-                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/strangeGhost.png"));
-
-            Image monsterImage = new Image(possibleMonsterList.get(typeMonster));
-            gc.drawImage(monsterImage,950,450);
-            gc.fillText(opponent.getName(),975,425);
-        }else{
-            //draw wizard
-            Image wizardImage = new Image("file:doc/images/gfx/gfx/fightScene/wizardRdyToFight.png");
-            gc.drawImage(wizardImage,900,390);
-            gc.fillText(opponent.getName(),960,350);
-        }
+        drawOpponent();
 
         //Draw Hero
         Image myHero = new Image("file:doc/images/gfx/gfx/fightScene/heroRdyToFight.png");
         gc.drawImage(myHero,-100,275);
 
-        //Draw item Hero Image
-        Image healPotionImage = new Image("file:doc/images/gfx/gfx/item/hpPotion.png");
-        gc.drawImage(healPotionImage,20,740);
-
-        Image antidotePotionImage = new Image("file:doc/images/gfx/gfx/item/antidotePotion.png");
-        gc.drawImage(antidotePotionImage,20,710);
-
-        Image energyDrinkPotionImage = new Image("file:doc/images/gfx/gfx/item/energyDrinkPotion.png");
-        gc.drawImage(energyDrinkPotionImage,20,675);
-
-        //Draw item stats
-        myFontStats = Font.font("Arial", FontWeight.BOLD, 17);
-        gc.setFont(myFontStats);
-        gc.setFill(Color.WHITE);
-        //Search all values
-        ArrayList<Integer> numberOfItem = new ArrayList<>();
-        ArrayList<Item> itemType = new ArrayList<>();
-        for(Map.Entry<Item,Integer> entry: hero.getMyItem().entrySet()){
-            numberOfItem.add(entry.getValue());
-            itemType.add(entry.getKey());
-        }
-
-        //Print all values
-        int yPositionItemNumber = 762;
-        for(int i = 0; i < 3;i++){
-            gc.fillText(String.format("= %d",numberOfItem.get(i)),50,yPositionItemNumber);
-            yPositionItemNumber -= 35;
-        }
-
-        //Draw Button to use potion (Heal/Antidote/EnergyDrink)
-        ItemButton healUseButton = new ItemButton(itemType.get(2),this,hero);
-        ItemButton antidoteUseButton = new ItemButton(itemType.get(1),this,hero);
-        ItemButton energyDrinkUseButton = new ItemButton(itemType.get(0),this,hero);
-        VBox itemUseBox = new VBox(healUseButton,antidoteUseButton,energyDrinkUseButton);
-        itemUseBox.setSpacing(8);
-        itemUseBox.setTranslateX(-495);
-        itemUseBox.setTranslateY(675);
-
-        root.getChildren().addAll(itemUseBox);
-        this.setRoot(root);
+        drawItem();
     }
 
     /**
@@ -300,6 +225,104 @@ public class FightScene extends GeneralScene{
         }
     }
 
+    /**
+     * Draw the opponent, the image depend on the type of the enemy.
+     */
+    public void drawOpponent(){
+        //Draw Monster
+        if(opponent instanceof Monster){
+            ArrayList<String> possibleMonsterList = new ArrayList<>(List.of(
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/chainBeast.png",
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/dracomachina.png",
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/eelWielder.png",
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/fishbone.png",
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/shredSquid.png",
+                    "file:doc/images/gfx/gfx/fightScene/extraCreatures/strangeGhost.png"));
+
+            Image monsterImage = new Image(possibleMonsterList.get(typeMonster));
+            gc.drawImage(monsterImage,950,450);
+            gc.fillText(opponent.getName(),975,425);
+        }else{
+            //draw wizard
+            Image wizardImage = new Image("file:doc/images/gfx/gfx/fightScene/wizardRdyToFight.png");
+            gc.drawImage(wizardImage,900,390);
+            gc.fillText(opponent.getName(),960,350);
+        }
+    }
+
+    /**
+     * Draw hero stats (hp,power,status).
+     */
+    public void drawHeroStats(){
+        //Draw Hero Stats
+        Image hpImage = new Image("file:doc/images/gfx/gfx/fightScene/hpImage.png");
+        Image swordImage = new Image("file:doc/images/gfx/gfx/fightScene/swordnormal.png");
+        Image statusImage = new Image("file:doc/images/gfx/gfx/fightScene/strenght.png");
+        gc.drawImage(hpImage,20,50);
+        gc.drawImage(swordImage,20,100);
+        gc.drawImage(statusImage,20,150);
+
+        gc.setFill(Color.WHITE);
+        gc.fillText(String.format("%2d",hero.getHp()),65,75);
+        gc.fillText(String.format("%d",hero.getPower()),65,122);
+        gc.fillText(hero.getCurrentStatus().getNameState(),65,175);
+        gc.fillText(hero.getName(), 170,350);
+
+        //Draw opponent Stats
+        gc.drawImage(hpImage,1135,50);
+        gc.drawImage(swordImage,1135,100);
+        gc.drawImage(statusImage,1135,150);
+
+        gc.fillText(String.format("%2d",opponent.getHp()),1095,75);
+        gc.fillText(String.format("%d",opponent.getPower()),1095,122);
+        gc.fillText(opponent.getCurrentStatus().getNameState(),1020,175);
+    }
+
+    /**
+     * Draw the item and all item stats.
+     */
+    public void drawItem(){
+        //Draw item Hero Image
+        Image healPotionImage = new Image("file:doc/images/gfx/gfx/item/hpPotion.png");
+        gc.drawImage(healPotionImage,20,740);
+
+        Image antidotePotionImage = new Image("file:doc/images/gfx/gfx/item/antidotePotion.png");
+        gc.drawImage(antidotePotionImage,20,710);
+
+        Image energyDrinkPotionImage = new Image("file:doc/images/gfx/gfx/item/energyDrinkPotion.png");
+        gc.drawImage(energyDrinkPotionImage,20,675);
+
+        //Draw item stats
+        Font myFontStats = Font.font("Arial", FontWeight.BOLD, 17);
+        gc.setFont(myFontStats);
+        gc.setFill(Color.WHITE);
+        //Search all values
+        ArrayList<Integer> numberOfItem = new ArrayList<>();
+        ArrayList<Item> itemType = new ArrayList<>();
+        for(Map.Entry<Item,Integer> entry: hero.getMyItem().entrySet()){
+            numberOfItem.add(entry.getValue());
+            itemType.add(entry.getKey());
+        }
+
+        //Print all values
+        int yPositionItemNumber = 762;
+        for(int i = 0; i < 3;i++){
+            gc.fillText(String.format("= %d",numberOfItem.get(i)),50,yPositionItemNumber);
+            yPositionItemNumber -= 35;
+        }
+
+        //Draw Button to use potion (Heal/Antidote/EnergyDrink)
+        ItemButton healUseButton = new ItemButton(itemType.get(2),this,hero);
+        ItemButton antidoteUseButton = new ItemButton(itemType.get(1),this,hero);
+        ItemButton energyDrinkUseButton = new ItemButton(itemType.get(0),this,hero);
+        VBox itemUseBox = new VBox(healUseButton,antidoteUseButton,energyDrinkUseButton);
+        itemUseBox.setSpacing(8);
+        itemUseBox.setTranslateX(-495);
+        itemUseBox.setTranslateY(675);
+
+        root.getChildren().addAll(itemUseBox);
+        this.setRoot(root);
+    }
     /**
      * Fade Transition for a text
      * @param myText the text to show
