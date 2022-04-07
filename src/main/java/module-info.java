53f3ce565a508bda7c5452c7496778d0402dbd45
Michael Bardou <michael.bardou@icloud.com>
2022-04-07 18:51:01 +0200

    remove usless dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,12 @@
 module com.labygame{
     requires static lombok;
     requires javafx.controls;
-    requires javafx.fxml;
     requires org.jetbrains.annotations;
     requires javafx.media;
     requires javafx.graphics;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
 
-    opens com.labygame.front to javafx.fxml;
-    opens com.labygame.front.scenes to javafx.fxml;
     exports com.labygame.front;
     exports com.labygame.front.scenes;
     exports com.labygame.personnage;
